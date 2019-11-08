45c5b18a6980274544d3573d1c1d22ba283b3ca7
okutani-t <okutani.nt@gmail.com>
2019-11-08 17:59:28 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# webpack-react-typescript-sample
+
+# How To Run
+
+`$ yarn run start`
+
+# How To Build
+
+`$ yarn run build`
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  devServer: {
+    contentBase: 'dist',
+    open: true
+  }
+}
